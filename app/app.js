8d1aac2241e4d851bc371a0769ef9411d8406122
Swapnil Solanki <swapnil.solanki@townscript.com>
2017-08-09 18:32:06 +0530

    removed view1,2

--- app/app.js
+++ app/app.js
@@ -3,8 +3,6 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
   'myApp.login',
   'myApp.signup',
   'myApp.home',
@@ -24,4 +22,3 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
 		alert("Hello World");
 	});
 })();*/
-
