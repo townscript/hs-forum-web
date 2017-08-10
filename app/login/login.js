c82352c731096c65b55fe3715d56d3a5e9231def
Swapnil Solanki <swapnil.solanki@townscript.com>
2017-08-10 12:29:06 +0530

    bower version modified

--- app/login/login.js
+++ app/login/login.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.login', ['ngRoute'])
+angular.module('myApp.login', ['ngRoute','ngResource'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/login', {
@@ -9,7 +9,38 @@ angular.module('myApp.login', ['ngRoute'])
   });
 }])
 
-.controller('loginCtrl', [function() {
+.controller('loginCtrl', ['$location','userData', '$scope', '$http', function login($location, userData, $scope, $http) {
 
-	
+$scope.user={};
+$scope.success=true;
+
+//$scope.login = function() {
+//	console.log($scope.user);
+
+var loginUrl="http://localhost:8080/rest/login/checkLogin?dataJson=";
+var dataJson="{\"userName\":\""+$scope.user.uname+"\",\"password\":\""+$scope.user.pwd+"\"}";
+
+$http({method: "POST", url: loginUrl+dataJson, 
+	headers: {'Content-Type': 'application/x-www-form-urlencoded','Access-Control-Allow-Origin':'*'}
+})
+.then(function(response) {
+		  console.log(response);
+		  alert(response);
+          $scope.res = response.data;
+          userData.set($scope.res);
+          console.log($scope.res.username);
+          if(typeof $scope.res.username === "string"){
+            console.log($scope.res.username);
+            $location.path('/home');
+
+          } else{
+            $scope.success = false;
+          }
+        }, function(response) {
+          console.log(response);
+      });
+   
+//};
+
 }]);
+
