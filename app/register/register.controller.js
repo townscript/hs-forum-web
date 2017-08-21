748ae40e3cd85fb318ce85dbe0263088e8522afe
Swapnil Solanki <swapnil.solanki@townscript.com>
2017-08-21 15:26:32 +0530

    email/mobile validation added

--- app/register/register.controller.js
+++ app/register/register.controller.js
@@ -7,9 +7,14 @@
 
     RegisterController.$inject = ['$location', '$scope', '$http'];
     function RegisterController($location, $scope, $http) {
+
+        $scope.emailFormat = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
+        $scope.mobileFormat = /^((\+?(\d{2})([-]|[ ])?)|[0]?)?\d{10}$/;
+
         //alert("inside register");
         var vm = this;
         vm.register = register;
+        vm.validatePwd = validatePwd;
 
         function register() {
             $scope.success = true;
@@ -17,12 +22,8 @@
             vm.dataLoading = true;
 
             var signupUrl="http://localhost:8080/rest/user/newUser?dataJson=";
-
-            //alert("username: "+vm.user.username + " ;password: "+vm.user.password);
-            //var dataJson="{\"userName\":\""+vm.user.username+"\",\"password\":\""+vm.user.password+"\"}";
+            
             var dataJson="{\"userName\":\""+vm.user.username+"\",\"userPassword\":\""+vm.user.password+"\",\"userEmail\":\""+vm.user.email+"\",\"userMobile\":\""+vm.user.mobile+"\",\"userPropic\":\"\"}";
-
-
 
             $http({method: "POST", url: signupUrl+dataJson, 
                 headers: {'Access-Control-Allow-Origin':'*'}
@@ -30,7 +31,6 @@
             .then(function(response) {
                   //console.log(response);
                   //alert("response.data: "+JSON.stringify(response.data));
-                  //alert("response.data: "+response.data);
                   if(response.data){
                     $location.path('/login');
 
@@ -47,9 +47,19 @@
         };
 
         function validatePwd(){
-            alert("inside register.validatePwd: ");
-           // alert("pwd: "+vm.user.password);
-
+            var pwd1= vm.user.password
+            var pwd2= vm.user.password2
+            if( pwd1 == null &&  pwd2 == null ) {
+              alert("Passwords should not be empty!");
+            } else if( pwd1 == null ) {
+              alert("Password should not be empty!");
+            } else if( pwd2 == null ) {
+              alert("Password2 should not be empty!");
+            } else if ( pwd1 != pwd2 ) {
+              alert("Both paswords should be same!");
+              vm.user.password = "";
+              vm.user.password2 = "";
+            }
         }
 
     }
